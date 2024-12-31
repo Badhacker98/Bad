bdbbd5b35b4ad54fea4e5b76f30102d18b6bb16b
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-12-31 20:14:48 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -101,7 +101,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/Badhacker98/BADMUSIC",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Bad")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Testing")
 
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv(
